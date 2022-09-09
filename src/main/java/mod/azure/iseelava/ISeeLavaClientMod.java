3ecec58e91dcb588f84b4b2c2467764d299e2f08
Sea-Kerman <odyssea.fll@gmail.com>
2022-09-08 18:25:29 -0700

    Swap render layer to cutout
    looks a bit worse aesthetically, but allows better visibility and shader compatibility

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -12,7 +12,7 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putFluid(Fluids.LAVA, RenderLayer.getTranslucent());
-		BlockRenderLayerMap.INSTANCE.putFluid(Fluids.FLOWING_LAVA, RenderLayer.getTranslucent());
+		BlockRenderLayerMap.INSTANCE.putFluid(Fluids.LAVA, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putFluid(Fluids.FLOWING_LAVA, RenderLayer.getCutout());
 	}
 }
